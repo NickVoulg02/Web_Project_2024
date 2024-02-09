a5aa21d16ca34d663d06f8aaa4807d4614301294
Nikolaos_Voulgaris <83508203+NickVoulg02@users.noreply.github.com>
2024-02-09 19:56:44 +0200

    Delete map_erotimata directory

--- map_erotimata/updateBase.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'db_connect.php';
-
-$longtitude = $_POST['longtitude'];
-$latitude = $_POST['latitude'];
-
-$query = 'UPDATE base SET longtitude='. $longtitude.',latitude='.$latitude.'WHERE id=0';
-$stmt = $pdo-> prepare($query);
-$stmt-> execute();
